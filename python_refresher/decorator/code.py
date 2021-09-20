b6ac9e7972332c8344a7294b79719681c7c32a95
Farhad Mani <frhdmani@gmail.com>
2021-09-20 11:33:29 +0100

    add decorating a funtion with parameters

--- python_refresher/decorator/code.py
+++ python_refresher/decorator/code.py
@@ -10,20 +10,24 @@ def make_secure(func: Callable) -> Callable:
     """secure the func """
 
     @functools.wraps(func)
-    def secure_function():
+    def secure_function(*args, **kwargs):
         if user["access_level"] == "admin":
-            return func()
+            return func(*args, **kwargs)
         return f"No admin access available for {user['user_name']}."
     return secure_function
 
 
 @make_secure
-def get_admin_password():
+def get_admin_password(panel:str) -> str:
     """get the password from the admin"""
-    return "12345"
+    if panel == "admin":
+        return "12345"
+    if panel == "billing":
+        return "super secure password"
+    return "not authorized panel"
 
 
-print(get_admin_password())
+print(get_admin_password("billing"))
 
 print(get_admin_password.__name__)
 
