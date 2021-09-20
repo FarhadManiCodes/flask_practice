1871a63ca5728603d6464a8c0768d146732bb6c9
Farhad Mani <frhdmani@gmail.com>
2021-09-20 11:45:53 +0100

    Add parameter to the decorator

--- python_refresher/decorator/code.py
+++ python_refresher/decorator/code.py
@@ -2,22 +2,21 @@
 from typing import Callable
 import functools
 
-user = {"user_name": "Farhad",
-        "access_level": "guest"}
+
+def make_secure(access_level:str) -> Callable:
+    """secure decorator"""
+    def decorators(func: Callable) -> Callable:
+        """secure the func """
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            return f"No {access_level} access available for {user['user_name']}."
+        return secure_function
+    return decorators    
 
 
-def make_secure(func: Callable) -> Callable:
-    """secure the func """
-
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        if user["access_level"] == "admin":
-            return func(*args, **kwargs)
-        return f"No admin access available for {user['user_name']}."
-    return secure_function
-
-
-@make_secure
+@make_secure("admin")
 def get_admin_password(panel:str) -> str:
     """get the password from the admin"""
     if panel == "admin":
@@ -27,8 +26,11 @@ def get_admin_password(panel:str) -> str:
     return "not authorized panel"
 
 
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+
+user = {"user_name": "Farhad", "access_level": "admin"}
+print(get_dashboard_password())
 print(get_admin_password("billing"))
-
-print(get_admin_password.__name__)
-
-print(get_admin_password.__doc__)
